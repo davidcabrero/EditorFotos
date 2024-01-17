c1933205f6f1bcbe6467d67b21a87b5911820a54
davidcj <d.cabrero.2023@gmail.com>
2024-01-17 19:35:01 +0100

    Corregido y mejorado transform_multi

--- transform_multi.py
+++ transform_multi.py
@@ -9,16 +9,18 @@ def transform():
     # Obtener todas las funciones y argumentos
     funcs_args = sys.argv[2:]
 
-    for i in range(0, len(funcs_args), 2):
+    for i in range(0, len(funcs_args)):
         # Iterar de 2 en 2 para obtener funciones y argumentos multiples en cualquier orden
 
         func = funcs_args[i]
         args = funcs_args[i + 1:]
-
+        # Menu de funciones con los argumentos según donde vaya iterando el bucle for
         if func == "change_colors":
-            pixels = change_colors(pixels, *args)
+            pixels_old = (int(funcs_args[i + 1]), int(funcs_args[i + 2]), int(funcs_args[i + 3]))
+            pixels_new = (int(funcs_args[i + 4]), int(funcs_args[i + 5]), int(funcs_args[i + 6]))
+            pixels = change_colors(pixels, pixels_old, pixels_new)
         elif func == "rotate_colors":
-            pixels = rotate_colors(pixels, *args)
+            pixels = rotate_colors(pixels, int(funcs_args[i + 1]))
         elif func == "rotate_right":
             pixels = rotate_right(pixels)
         elif func == "mirror":
@@ -28,13 +30,12 @@ def transform():
         elif func == "grayscale":
             pixels = grayscale(pixels)
         elif func == "filter":
-            pixels = filter(pixels, *args)
+            pixels = filter(pixels, float(funcs_args[i + 1]), float(funcs_args[i + 2]), float(funcs_args[i + 3]))
         elif func == "crop":
-            pixels = crop(pixels, *args)
+            pixels = crop(pixels, int(funcs_args[i + 1]), int(funcs_args[i + 2]), int(funcs_args[i + 3]),
+                          int(funcs_args[i + 4]))
         elif func == "shift":
-            pixels = shift(pixels, *args)
-        else:
-            print("Función inválida")
+            pixels = shift(pixels, int(funcs_args[i + 1]), int(funcs_args[i + 2]))
 
     write_img(pixels, sys.argv[1].split(".")[0] + "_trans." + sys.argv[1].split(".")[1])
 
